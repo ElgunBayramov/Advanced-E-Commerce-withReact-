dfc02a965c8398ecf8082fc3312efa0975dc8ef7
Elguncode <elgunmb@code.edu.az>
2025-11-23 20:05:58 +0100

    server js added

---
+++ server.js
@@ -0,0 +1,13 @@
+import jsonServer from "json-server";
+
+const server = jsonServer.create();
+const router = jsonServer.router("json-server/db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+const port = process.env.PORT || 4000;
+server.listen(port, () => {
+  console.log("JSON Server is running on port " + port);
+});
