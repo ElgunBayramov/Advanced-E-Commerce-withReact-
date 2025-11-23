5359390838727cba99d0a105b53852acd8465860
Elguncode <elgunmb@code.edu.az>
2025-11-23 20:39:32 +0100

    jsonserver redirected

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 import jsonServer from "json-server";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const dbFile = path.join(__dirname, "jsonserver", "db.json");
 
 const server = jsonServer.create();
-const router = jsonServer.router("jsonserver/db.json");
+const router = jsonServer.router(dbFile);
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
